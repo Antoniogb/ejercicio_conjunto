89bc56f23896128fc4b0dc516fd9574347104599
“Antonio” <dantegb2@gmail.com>
2022-10-28 11:46:09 +0200

    add date

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
+from datetime import date
+from datetime import datetime
+
+
 print ("Hello everybody!")
+
+#Día actual
+today = date.today()
+
+#Fecha actual
+now = datetime.now()
+
+print(today)
+print(now)
